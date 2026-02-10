08f55988a585253d94defdf36664f5569632c3ee
Max <max@guavaapps.com>
2026-02-10 03:50:10 +0000

    debug logs

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ import {HttpError} from "koa";
 import mongoose from "mongoose";
 import {User, UserPrivate, UserPublic} from "../schemas/user.js";
 import {hash_password} from "../passwords.js";
+import {Sanitized} from "../schema_utils.js";
+import {bodyParser} from "@koa/bodyparser";
 
 export {
     userRouter
@@ -11,6 +13,8 @@ export {
 const userRouter = new Router({
     prefix: "/users",
 });
+
+userRouter.use(bodyParser())
 
 userRouter.get("/:id", async (ctx) => {
     const queryParams = ctx.request.params
@@ -30,5 +34,55 @@ userRouter.get("/:id", async (ctx) => {
         ctx.state.response.body = user;
     } catch (err) {
         ctx.throw(404, "User not found.")
+    }
+})
+
+userRouter.put("/:id", async (ctx) => {
+    const queryParams = ctx.request.params
+    const body = ctx.request.body
+
+    let userId = queryParams.id
+
+    if (!userId) {
+        ctx.throw(404, "User not found.")
+    }
+
+    if (!body) {
+        console.log("No body, 200")
+        ctx.state.response.status = 200;
+        return
+    }
+
+    console.log(`[PUT] user userId=${userId}`)
+
+    let update = Sanitized(User.schema, body, ["id", "dateJoined", "pictureUrl", "password"]);
+
+    try {
+        await User.updateOne({id: userId}, update);
+    } catch (e) {
+        let code = e.code
+
+        if (code === 11000) {
+            // duplicate
+            let field = Object.keys(e.keyPattern)[0]
+
+            switch (field) {
+                case "username":
+                    ctx.state.response.body.error.message = "An account with this username already exists."
+                    ctx.throw(409, "An account with this username already exists.")
+                    break;
+                case "email":
+                    ctx.state.response.body.error.message = "An account with this email already exists."
+                    ctx.throw(409, "An account with this email already exists.")
+                    break;
+                default:
+                    ctx.throw(409, "An error occurred.")
+            }
+        }
+        else {
+            console.log(e)
+        }
+
+        ctx.state.response.status = 200
     }
 })
